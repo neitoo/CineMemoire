e19ce9d2703f9e544ee8a81245097b7f2dd5bc2b
neitoo <samuraa855@gmail.com>
2024-01-23 19:50:15 +0500

    Обновлен дизайн главного экрана

--- app/src/main/java/com/neito/cinememoire/screens/MainScreen.kt
+++ app/src/main/java/com/neito/cinememoire/screens/MainScreen.kt
@@ -1,8 +1,16 @@
 package com.neito.cinememoire.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,9 +19,12 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -40,10 +51,16 @@ fun MainScreen(mainNavController: NavHostController){
         },
         bottomBar = {
             BottomBarContent(navController = navController)
-        },
-
+        }
     ) {
-        BottomNavGraph(navController = navController)
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .padding(it),
+            shape = RoundedCornerShape(20.dp),
+            color = MaterialTheme.colorScheme.inverseOnSurface,
+        ) {
+            BottomNavGraph(navController = navController)
+        }
     }
 }
 
@@ -75,7 +92,8 @@ fun BottomBarContent(navController: NavHostController){
     val currentDestination = navBackStackEntry?.destination
 
     NavigationBar(
-        modifier = Modifier.height(60.dp)
+        modifier = Modifier.height(60.dp),
+        tonalElevation = 0.dp
     ) {
         listOfNavItems.forEach { screens ->
             NavigationBarItem(
