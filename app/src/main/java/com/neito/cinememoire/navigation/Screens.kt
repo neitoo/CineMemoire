0e40bfb922873da07a23f9d66ceb06018ba2b78d
neitoo <samuraa855@gmail.com>
2024-02-11 17:46:10 +0500

    Переписан поиск пути до экрана

--- app/src/main/java/com/neito/cinememoire/navigation/Screens.kt
+++ app/src/main/java/com/neito/cinememoire/navigation/Screens.kt
@@ -1,10 +1,9 @@
 package com.neito.cinememoire.navigation
-
-enum class Screens {
-    HomeScreen,
-    SearchScreen,
-    WishlistScreen,
-    SettingsScreen,
-    MainScreen,
-    CreateScreens
+sealed class Screens(val screen: String){
+    data object HomeScreen: Screens("home")
+    data object SearchScreen: Screens("search")
+    data object WishlistScreen: Screens("wishlist")
+    data object SettingsScreen: Screens("settings")
+    data object MainScreen: Screens("main");
+    data object CreateScreen: Screens("create")
 }
