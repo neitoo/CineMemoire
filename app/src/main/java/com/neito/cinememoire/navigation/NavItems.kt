26943f0f5a9b4cef6fb23675fbe3f718fe9bfc2b
neitoo <samuraa855@gmail.com>
2024-02-11 17:45:56 +0500

    Переписан поиск пути до экрана

--- app/src/main/java/com/neito/cinememoire/navigation/NavItems.kt
+++ app/src/main/java/com/neito/cinememoire/navigation/NavItems.kt
@@ -12,16 +12,16 @@ val listOfNavItems = listOf(
     NavItem(
         title = R.string.home_name,
         icon = R.drawable.a_home_icon,
-        route = Screens.HomeScreen.name
+        route = Screens.HomeScreen.screen
     ),
     NavItem(
         title = R.string.search_name,
         icon = R.drawable.a_search_icon,
-        route = Screens.SearchScreen.name
+        route = Screens.SearchScreen.screen
     ),
     NavItem(
         title = R.string.wishlist_name,
         icon = R.drawable.a_wishlist_icon,
-        route = Screens.WishlistScreen.name
+        route = Screens.WishlistScreen.screen
     ),
 )
