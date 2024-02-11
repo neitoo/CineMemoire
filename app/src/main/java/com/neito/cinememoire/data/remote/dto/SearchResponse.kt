7bfa65435bc0303944e8db50b9bff01c86ace1b1
neitoo <samuraa855@gmail.com>
2024-02-11 17:50:34 +0500

    создан дата класс для апи поиска

---
+++ app/src/main/java/com/neito/cinememoire/data/remote/dto/SearchResponse.kt
@@ -0,0 +1,38 @@
+package com.neito.cinememoire.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchResponse(
+    val keyword: String,
+    val pagesCount: Long,
+    val films: List<Film>,
+    val searchFilmsCountResult: Long
+)
+
+@Serializable
+data class Film (
+    val filmId: Long,
+    val nameRu: String? = null,
+    val nameEn: String,
+    val type: String,
+    val year: String,
+    val description: String? = null,
+    val filmLength: String? = null,
+    val countries: List<CountryS>,
+    val genres: List<GenreS>,
+    val rating: String,
+    val ratingVoteCount: Long,
+    val posterUrl: String,
+    val posterUrlPreview: String
+)
+
+@Serializable
+data class CountryS (
+    val country: String
+)
+
+@Serializable
+data class GenreS (
+    val genre: String
+)
