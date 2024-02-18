64cc5af5dbbff1f9e9073a5ea366bb94f0fd3920
neitoo <samuraa855@gmail.com>
2024-02-18 17:47:43 +0500

    Код переписан под открытие BottomSheet

--- app/src/main/java/com/neito/cinememoire/screens/MainScreen.kt
+++ app/src/main/java/com/neito/cinememoire/screens/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.neito.cinememoire.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,41 +12,57 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.neito.cinememoire.R
 import com.neito.cinememoire.navigation.Screens
 import com.neito.cinememoire.navigation.listOfNavItems
+import com.neito.cinememoire.presentation.BottomSheetContent
+import com.neito.cinememoire.presentation.BottomSheetViewModel
+import com.neito.cinememoire.presentation.componentes.CreationContent
+import com.neito.cinememoire.presentation.componentes.SettingsContent
+import org.koin.compose.koinInject
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(navController: NavHostController){
-    val navControllerBottomBar = rememberNavController()
+    val sheetState = rememberModalBottomSheetState()
+    val viewModel = koinInject<BottomSheetViewModel>()
+
     Scaffold(
         topBar = {
-            TopAppBarContent(navController)
+            TopAppBarContent(viewModel)
         },
         bottomBar = {
-            BottomBarContent(navControllerBottomBar)
+            BottomBarContent(navController)
         }
     ) {
         Surface(modifier = Modifier
@@ -54,25 +71,51 @@ fun MainScreen(navController: NavHostController){
             shape = RoundedCornerShape(20.dp),
             color = MaterialTheme.colorScheme.inverseOnSurface,
         ) {
-            BottomNavGraph(navControllerBottomBar, navController)
+            NavGraph(navController,viewModel)
+        }
+    }
+
+    if (viewModel.showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                viewModel.showBottomSheet = false
+                viewModel.bottomSheetContent = null },
+            sheetState = sheetState
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp, vertical = 5.dp),
+                contentAlignment = Alignment.Center
+            ){
+                viewModel.bottomSheetContent?.let { content ->
+                    when (content) {
+                        is BottomSheetContent.SettingContent -> {
+                            SettingsContent()
+                        }
+                        is BottomSheetContent.CreateContent -> {
+                            CreationContent()
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
 
 @Composable
-private fun BottomNavGraph(
-    navControllerBottomBar: NavHostController,
-    navController: NavHostController
+private fun NavGraph(
+    navController: NavHostController,
+    viewModel: BottomSheetViewModel
 ) {
     NavHost(
-        navController = navControllerBottomBar,
+        navController = navController,
         startDestination = Screens.HomeScreen.screen,
     ) {
         composable(route = Screens.HomeScreen.screen) {
             HomeScreen()
         }
         composable(route = Screens.SearchScreen.screen) {
-            SearchScreen(navController)
+            SearchScreen(navController, viewModel)
         }
         composable(route = Screens.WishlistScreen.screen) {
             WishlistScreen()
@@ -82,7 +125,9 @@ private fun BottomNavGraph(
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun TopAppBarContent(navController: NavHostController) {
+private fun TopAppBarContent(
+    viewModel: BottomSheetViewModel
+) {
     TopAppBar(
         title = {
             Text(
@@ -92,7 +137,12 @@ private fun TopAppBarContent(navController: NavHostController) {
             )
         },
         actions = {
-            IconButton(onClick = { navController.navigate(Screens.SettingsScreen.screen)}) {
+            IconButton(
+                onClick = {
+                    viewModel.showBottomSheet = true
+                    viewModel.bottomSheetContent = BottomSheetContent.SettingContent
+                }
+            ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.a_settings_icon),
                     contentDescription = "Localized description"
