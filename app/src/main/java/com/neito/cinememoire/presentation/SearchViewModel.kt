8a22e8883da7db3847af503625eee870f44b928e
neitoo <samuraa855@gmail.com>
2024-02-20 21:36:31 +0500

    Добавлены переменные состояния поля поиска (исправлено сохранение состояния поля при переключении на другой экран)

--- app/src/main/java/com/neito/cinememoire/presentation/SearchViewModel.kt
+++ app/src/main/java/com/neito/cinememoire/presentation/SearchViewModel.kt
@@ -1,15 +1,21 @@
 package com.neito.cinememoire.presentation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.neito.cinememoire.data.remote.ApiService
 import com.neito.cinememoire.data.remote.dto.SearchResponse
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SearchViewModel(private val apiService: ApiService) : ViewModel() {
+    var textState by mutableStateOf("")
+    var previousSearchText by mutableStateOf("")
+
     private val _searchResults = MutableStateFlow<SearchResponse?>(null)
     val searchResults: StateFlow<SearchResponse?> = _searchResults
 
